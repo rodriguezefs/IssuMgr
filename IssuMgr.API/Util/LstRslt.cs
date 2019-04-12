46f6f6951e6534af1f83510ab6b27fbdeef7f142
rodriguezefs <rodriguezefs@live.com>
2019-04-12 09:51:12 -0400

    LstRslt

---
+++ IssuMgr.API/Util/LstRslt.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IssuMgr.API.Util {
+    public class LstRslt<T> where T : class {
+        public LstRslt() { }
+
+        public LstRslt(List<T> lst, Exception err) {
+            Lst = lst;
+            Err = err;
+        }
+
+        public LstRslt(List<T> lst) {
+            Lst = lst;
+            Err = null;
+        }
+        public List<T> Lst { get; set; }
+        public Exception Err { get; set; }
+    }
+}
--- IssuMgr.API/Util/SnglRslt.cs
+++ IssuMgr.API/Util/SnglRslt.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace IssuMgr.API.Util {
     public class SnglRslt<T> where T : class {
-        public SnglRslt() {
+        public SnglRslt() { }
 
-        }
         public SnglRslt(T sngl, Exception err) {
             Sngl = sngl;
             Err = err;
