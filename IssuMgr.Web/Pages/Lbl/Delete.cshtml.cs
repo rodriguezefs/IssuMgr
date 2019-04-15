73921d888b76fb6ebf053791cabe123a1b598afb
rodriguezefs <rodriguezefs@live.com>
2019-04-15 14:08:16 -0400

    Edit, Delete

---
+++ IssuMgr.Web/Pages/Lbl/Delete.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using IssuMgr.BO.Interfaces;
+using IssuMgr.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
+
+namespace IssuMgr.Web.Pages.Lbl {
+    public class DeleteModel: PageModel {
+        private readonly ILblBO LblBO;
+        public DeleteModel(ILblBO lblBO) {
+            LblBO = lblBO;
+        }
+
+        [BindProperty]
+        public LblModel Lbl { get; set; }
+        public async Task<ActionResult> OnGet(int id) {
+            var lxRslt = await LblBO.Get(id);
+            if(lxRslt.Err != null) {
+                TempData["MsgErr"] = lxRslt.Err.Message;
+
+                return Page();
+            }
+            return Page();
+        }
+
+        public async Task<ActionResult> OnPost(int id) {
+            var lxRslt = await LblBO.Delete(id);
+
+            if(lxRslt.Err != null) {
+                TempData["MsgErr"] = lxRslt.Err.Message;
+
+                return Page();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ IssuMgr.Web/Pages/Lbl/Edit.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using IssuMgr.BO.Interfaces;
+using IssuMgr.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
+
+namespace IssuMgr.Web.Pages.Lbl {
+    public class EditModel: PageModel {
+        private readonly ILblBO LblBO;
+        public EditModel(ILblBO lblBO) {
+            LblBO = lblBO;
+        }
+
+        [BindProperty]
+        public LblModel Lbl { get; set; }
+        public async Task<ActionResult> OnGet(int id) {
+            var lxRslt = await LblBO.Get(id);
+            if(lxRslt.Err != null) {
+                TempData["MsgErr"] = lxRslt.Err.Message;
+
+                return Page();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPost(int id) {
+            //if(!ModelState.IsValid) {
+            //    return Page();
+            //}
+
+            var lxRslt = await LblBO.Update(id, Lbl);
+
+            if(lxRslt.Err != null) {
+                TempData["MsgErr"] = lxRslt.Err.Message;
+
+                return Page();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
