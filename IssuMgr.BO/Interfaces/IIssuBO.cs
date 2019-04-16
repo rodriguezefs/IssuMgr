1e49d0e5d76676e3ea4cfdb43274a9e0f92e1de7
rodriguezefs <rodriguezefs@live.com>
2019-04-16 18:01:26 -0400

    Issu Index Start

---
+++ IssuMgr.BO/Interfaces/IIssuBO.cs
@@ -0,0 +1,13 @@
+﻿using IssuMgr.Model;
+using System.Threading.Tasks;
+
+namespace IssuMgr.BO.Interfaces {
+    public interface IIssuBO {
+        Task<LstRslt<IssuModel>> GetAll();
+        Task<SnglRslt<IssuModel>> Get(int id);
+        Task<ExeRslt> Create(IssuModel Issu);
+        Task<bool> Exists(int id);
+        Task<ExeRslt> Delete(int id);
+        Task<ExeRslt> Update(int id, IssuModel Issu);
+    }
+}
---
+++ IssuMgr.Web/Pages/Issu/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using IssuMgr.BO.Interfaces;
+using IssuMgr.Model;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+
+namespace IssuMgr.Web.Pages.Issu {
+    public class IndexModel: PageModel {
+        private readonly IIssuBO IssuBO;
+
+        public IndexModel(IIssuBO issuBO) {
+            IssuBO = issuBO;
+        }
+
+        public List<IssuModel> Issus { get; set; }
+
+        public async void OnGet() {
+            var lxRslt = await IssuBO.GetAll();
+
+            if(lxRslt.Err != null) {
+                TempData["MsgErr"] = lxRslt.Err.Message;
+            }
+
+            Issus = lxRslt.Lst;
+        }
+    }
+}
