5dba0bba890668a90ae7d3883f881746560a94ba
eliseo-rodriguez <eliseo_rodriguez@latisinformatica.com>
2019-05-02 17:38:53 -0400

    Reestructuración de Index

---
+++ IssuMgr.Web/Pages/Issu/Details.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using IssuMgr.BO.Interfaces;
+using IssuMgr.Model;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+
+namespace IssuMgr.Web.Pages.Issu {
+    public class DetailsModel: PageModel {
+        private readonly IIssuBO IssuBO;
+
+        public DetailsModel(IIssuBO issuBO) {
+            IssuBO = issuBO;
+        }
+
+        public List<IssuModel> Issus { get; set; }
+
+        public async void OnGet() {
+            var lxRslt = await IssuBO.GetAll();
+
+            if(lxRslt.Err != null) {
+                TempData["ExErr"] = lxRslt.Err;
+            }
+
+            Issus = lxRslt.Lst;
+        }
+    }
+}
