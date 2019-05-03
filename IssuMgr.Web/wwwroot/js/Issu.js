421674cf3c47d4f38853f78b33d34e32a7e2dc00
rodriguezefs <rodriguezefs@live.com>
2019-05-03 07:54:22 -0400

    foreach (var lbl in Model.Issu.LstLbl) - break;

--- IssuMgr.Web/wwwroot/js/Issu.js
+++ IssuMgr.Web/wwwroot/js/Issu.js
@@ -1,5 +1,5 @@
 ﻿$(document).ready(function () {
-    $('#LstLbl').multiselect({
+    $('#Issu_LstLbl').multiselect({
         enableClickableOptGroups: true,
         includeSelectAllOption: true,
         selectAllText: 'Seleccionar todos',
@@ -12,18 +12,18 @@
     //$('#Issu_LstLbl').multiselect('selectAll', false);
     $('#Issu_LstLbl').multiselect('updateButtonText');
 
-    $('#Lbls').on('change', function () {
+    $('#Issu_LstLbl').on('change', function () {
         $("#LblInputs").empty();
-        $("#Lbls option:selected").each(function (i, op) {
+        $("#Issu_LstLbl option:selected").each(function (i, op) {
             let lxClr = $(op).attr('data-clr');
             let lxBkClr = $(op).attr('data-bkclr');
             let lxLblId = $(op).val();
             let lxLbl = $(op).text();
             let lxStr = '' +
-                '<input type="text" name="LstLbl[' + i + '].LblId" id="LstLbl_' + i + '__LblId" value = "' + lxLblId + '"/>' +
-                '<input type="text" name="LstLbl[' + i + '].Lbl" id="LstLbl_' + i + '__Lbl" value = "' + lxLbl + '"/>' +
-                '<input type="text" name="LstLbl[' + i + '].Clr" id="LstLbl_' + i + '__Clr" value = "' + lxClr + '"/>' +
-                '<input type="text" name="LstLbl[' + i + '].BkClr" id="LstLbl_' + i + '__BkClr" value = "' + lxBkClr + '"/>'
+                '<input type="hidden" name="LstLbl[' + i + '].LblId" id="LstLbl_' + i + '__LblId" value = "' + lxLblId + '"/>' +
+                '<input type="hidden" name="LstLbl[' + i + '].Lbl" id="LstLbl_' + i + '__Lbl" value = "' + lxLbl + '"/>' +
+                '<input type="hidden" name="LstLbl[' + i + '].Clr" id="LstLbl_' + i + '__Clr" value = "' + lxClr + '"/>' +
+                '<input type="hidden" name="LstLbl[' + i + '].BkClr" id="LstLbl_' + i + '__BkClr" value = "' + lxBkClr + '"/>'
             $("#LblInputs").append(lxStr);
         });
     });
